1a78199f1af881b67b9379502436872cbd92946c
kritika <kritikasri27@gmail.com>
2021-05-08 05:23:07 +0530

    Added schema,routes and database connection logic in backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv/config");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+  () => console.log("Database connected")
+);
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
