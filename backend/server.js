74477999920f42f0936bd26c42eb1867b42651e8
kritika <kritikasri27@gmail.com>
2021-05-08 06:06:33 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send("Hello to Fitness Tracker API");
+});
 mongoose.connect(
   process.env.MONGO_URI,
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
